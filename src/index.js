5417cef33f8a3931ed2c5bba51ad9b20855bbfe1
aborisenok <lesha_borisenok@mail.ru>
2018-09-30 21:10:30 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    var H = 50;
-    var Q = 25;
-    var D = 10;
-    var N = 5;
-    var P = 1;
-    var obj = {};
+    const H = 50;
+    const Q = 25;
+    const D = 10;
+    const N = 5;
+    const P = 1;
+    const obj = {};
+
     if(currency === 0){
       return obj;
     }
@@ -15,23 +16,23 @@ module.exports = function makeExchange(currency) {
   
     if(currency/H >= 1){
       obj.H = Math.floor(currency/H);
-      currency -= H*obj.H;
+      currency -= H * obj.H;
     }
     if(currency/Q >= 1){
       obj.Q = Math.floor(currency/Q);
-      currency -= Q*obj.Q;
+      currency -= Q * obj.Q;
     }
     if(currency/D >= 1){
       obj.D = Math.floor(currency/D);
-      currency -= D*obj.D;
+      currency -= D * obj.D;
     }
     if(currency/N >= 1){
       obj.N = Math.floor(currency/N);
-      currency -= N*obj.N;
+      currency -= N * obj.N;
     }
     if(currency/P >= 1){
       obj.P = Math.floor(currency/P);
-      currency -= P*obj.P;
+      currency -= P * obj.P;
     }
   
     return obj;
